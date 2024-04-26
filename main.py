647cac1e5134a339c9c6aeb92160272596703191
ml <>
2024-04-26 14:32:50 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from streamlit_oauth import OAuth2Component
+
+# Set environment variables
+kadiApiBaseURL = 'https://kadi4mat.iam.kit.edu'
+AUTHORIZE_URL = kadiApiBaseURL + '/oauth/authorize'
+TOKEN_URL = kadiApiBaseURL + '/oauth/token'
+REFRESH_TOKEN_URL = kadiApiBaseURL + '/oauth/token'
+REVOKE_TOKEN_URL = kadiApiBaseURL + '/oauth/revoke'
+CLIENT_ID = st.secrets['oauthClientID']
+CLIENT_SECRET = st.secrets['oauthClientSecret']
+REDIRECT_URI = 'https://kaditest.streamlit.app/component/streamlit_oauth.authorize_button/index.html'
+SCOPE = ''
+
+# Create OAuth2Component instance
+oauth2 = OAuth2Component(CLIENT_ID, CLIENT_SECRET, AUTHORIZE_URL, TOKEN_URL, REFRESH_TOKEN_URL, REVOKE_TOKEN_URL)
+
+result = oauth2.authorize_button("Authorize", REDIRECT_URI, SCOPE, height=600, width=800, use_container_width=True, extras_params={'response_type': 'code'})
+if result and 'token' in result:
+    # If authorization successful, save token in session state
+    st.session_state.token = result.get('token')
+    st.rerun()
